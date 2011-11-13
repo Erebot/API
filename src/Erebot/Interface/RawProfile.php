a1d56b7f3fc913aeaeca80abd853320c911a510b
François Poirotte <clicky@erebot.net>
2011-11-13 15:19:53 +0100

    More doc.

--- src/Erebot/Interface/RawProfile.php
+++ src/Erebot/Interface/RawProfile.php
@@ -16,6 +16,26 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * \brief
+ *      A raw profile.
+ *
+ * A raw profile is just an interface that provides numeric
+ * constants. The name of the constant is known as the "raw name"
+ * while the value of the constant is known as the "raw numeric"
+ * or "raw code".
+ *
+ * There is no need to provide concrete classes implementing
+ * the profiles. Instead, a raw profile loader is used
+ * (see Erebot_Interface_RawProfileLoader) to manage the
+ * profiles.
+ *
+ * Each IRC connection (Erebot_Connection) has a raw profile
+ * loader associated with it. To define a new profile, create
+ * a new interface that extends this one and adds constants,
+ * then register it through the raw profile loader associated
+ * with your IRC connection.
+ */
 interface Erebot_Interface_RawProfile
 {
 }
