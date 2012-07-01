02d1858050d5693f0098c7333d958e521903c627
François Poirotte <clicky@erebot.net>
2012-07-01 13:04:41 +0200

    Remove Erebot_Interface_BidirectionalConnection.
    It provided no real benefit over using Erebot_Interface_SendingConnection
    and Erebot_Interface_ReceivingConnection directly.

--- src/Erebot/Interface/IrcConnection.php
+++ src/Erebot/Interface/IrcConnection.php
@@ -16,18 +16,56 @@
     along with Erebot.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * \brief
+ *      Interface for IRC(S) connections.
+ *
+ * This interface provides the necessary methods
+ * to handle an IRC(S) connection.
+ */
 interface   Erebot_Interface_IrcConnection
 extends     Erebot_Interface_ModuleContainer,
             Erebot_Interface_EventDispatcher,
-            Erebot_Interface_BidirectionalConnection,
+            Erebot_Interface_SendingConnection,
+            Erebot_Interface_ReceivingConnection,
             Erebot_Interface_Collated
 {
+    /**
+     * Returns the raw profile loader associated with
+     * this connection.
+     *
+     * \retval Erebot_Interface_RawProfileLoader
+     *      The raw profile loader associated with this
+     *      connection.
+     */
     public function getRawProfileLoader();
 
+    /**
+     * Sets the new raw profile loader to use for this
+     * connection.
+     *
+     * \param Erebot_Interface_RawProfileLoader $loader
+     *      New raw profile loader to use for this
+     *      connection.
+     */
     public function setRawProfileLoader(
         Erebot_Interface_RawProfileLoader $loader
     );
 
+    /**
+     * Determines if the given string is a valid channel name or not.
+     * A channel name usually starts with the hash symbol (#).
+     * Valid characters for the rest of the name vary between IRC networks.
+     *
+     * \param $chan
+     *      Tentative channel name.
+     *
+     * \retval bool
+     *      TRUE if $chan is a valid channel name, FALSE otherwise.
+     *
+     * \throw Erebot_InvalidValueException
+     *      $chan is not a string or is empty.
+     */
     public function isChannel($chan);
 }
 
